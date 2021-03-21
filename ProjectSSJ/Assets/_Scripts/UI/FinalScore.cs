f99a203e11248acc64fc024f470f7add5e7a9588
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2021-03-21 19:56:04 -0300

    Score traking and bug tracking

--- ProjectSSJ/Assets/_Scripts/UI/FinalScore.cs
+++ ProjectSSJ/Assets/_Scripts/UI/FinalScore.cs
@@ -5,11 +5,31 @@ using UnityEngine.UI;
 
 public class FinalScore : MonoBehaviour
 {
-    [SerializeField] private Text finalScore = default;
-    [SerializeField] private Text score = default;
+    [SerializeField] private Text fireflyText = default;
+    [SerializeField] private Text cricketText = default;
+    [SerializeField] private Text beeText = default;
+    [SerializeField] private Text finalScoreText = default;
+
+    private int totalFly = 0;
+    private int totalCri = 0;
+    private int totalBee = 0;
+
+    private int finalScore = 0;
 
     private void OnEnable()
     {
-        finalScore.text=score.text; 
+        Debug.Log("FinalScore Enabled");
+
+        totalFly = Score.GetTotalFly();
+        totalCri = Score.GetTotalCri();
+        totalBee = Score.GetTotalBee();
+
+        finalScore = Score.GetFinalScore();
+
+        fireflyText.text = totalFly.ToString();
+        cricketText.text = totalCri.ToString();
+        beeText.text = totalBee.ToString();
+
+        finalScoreText.text = finalScore.ToString();
     }
 }
